a77f567eb9798112e5cdbfeb2800ff8e213d6b17
Fred Diego <fred@emersoncollective.com>
2021-04-29 16:39:17 -0500

    minor modification for easier reading

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-docker run -e CONFIG='development' -e SECRET_KEY='dev' --env-file .env --entrypoint "python" -p 5002:5000 -v $PWD:/app shipt-receipts-dev:latest -m pytest
+docker run -e CONFIG='development' \
+           -e SECRET_KEY='dev'     \
+           --env-file .env         \
+           --entrypoint "python"   \
+           -p 5002:5000                           \
+           -v $PWD:/app shipt-receipts-dev:latest \
+           -m pytest

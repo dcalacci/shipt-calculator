5225b08875e2f42dba9a3efca1e4f7c5dadeafbd
Dan Calacci <dcalacci@media.mit.edu>
2021-09-13 14:48:30 -0400

    Removes auth endpoints from main api module

--- run-dev.sh
+++ run-dev.sh
@@ -3,4 +3,4 @@
 # set env vars
 # build docker container
 # docker build -t "shipt-calculator-dev" -f Dockerfile.dev .
-docker run -e CONFIG='development' --env-file .env -p 5000:5000 -v $PWD:/app shipt-calculator-dev:latest
+docker run -e CONFIG='development' --env-file .env -p 5002:5000 -v $PWD:/app shipt-calculator-dev:latest
--- shipt/api.py
+++ shipt/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, g
 from twilio import twiml, base
 from twilio.rest import Client
-from auth import ensure_authenticated
+from auth import ensure_authenticated, bp
 import os
 import pyotp
 import bcrypt
@@ -22,42 +22,3 @@ def wrapped_route():
 @bp.route("/")
 def index():
     return "urls2 index route"
-
-
-def encode_base32(str):
-    """encode input as base32 string using input and a secret key"""
-    return base64.b32encode(bytearray(str + SECRET_KEY, "ascii")).decode("utf-8")
-
-
-def get_otp(phone):
-    """create OTP for a phone, using phone as base32 secret"""
-    totp = pyotp.TOTP(encode_base32(phone))
-    return totp.now()
-
-
-@bp.route("/auth_otp")
-def auth_phone():
-    """creates (and sends, through twilio) an OTP code for a phone"""
-    phone = request.form["phone"]
-    # create one-time password with phone as secret
-    message = client.messages.create(
-        body="Your one-time code from Shipt Calculator: {}".format(get_otp(phone)),
-        from_=os.environ["TWILIO_NUMBER"],
-        to=phone,
-    )
-    return jsonify(isError=False, message="Success", statusCode=200), 200
-
-
-@bp.route("/verify_otp", methods=["POST"])
-def verify_otp():
-    """returns 200 if the code matches the current OTP for the phone in the form"""
-    code = request.form.get("otp")
-    phone = request.form.get("phone")
-    authenticated = get_otp(phone) == code
-    if not authenticated:
-        return (
-            jsonify(isError=True, message="Authentication Failed", statusCode=400),
-            400,
-        )
-    else:
-        return jsonify(isError=False, message="Success", statusCode=200), 200
